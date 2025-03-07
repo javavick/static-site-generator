93157b8a00af279df7fbb102191fe7e6f7fb78d2
Vick Javariz <66493562+javavick@users.noreply.github.com>
2025-03-07 18:54:38 -0500

    Create HTMLNode class

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if len(self.props) == 0:
+            return ""
+        
+        prop_to_string = lambda prop: f'{prop[0]}="{prop[1]}"'
+        prop_strings = list(map(prop_to_string, self.props.items()))
+        return " ".join(prop_strings)
