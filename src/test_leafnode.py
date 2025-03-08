0b9ac090e3a8c06c477253b2d4296e0ef39a1433
Vick Javariz <66493562+javavick@users.noreply.github.com>
2025-03-08 12:39:11 -0500

    Create unit tests for leafnode.py

---
+++ src/test_leafnode.py
@@ -0,0 +1,77 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    # --- __init__() ---
+    def test_initialization_with_all_data(self):
+        """
+        Test that a LeafNode is initialized correctly with all data passed in.
+        """
+        node = LeafNode("p", "Test", {"class": "container"})
+        self.assertEqual(node.tag, "p")
+        self.assertEqual(node.value, "Test")
+        self.assertEqual(node.props, {"class": "container"})
+        self.assertIsNone(node.children)
+
+    def test_initialization_without_props(self):
+        """Test that a LeafNode is initialized correctly without props."""
+        node = LeafNode("div", "Test")
+        self.assertEqual(node.tag, "div")
+        self.assertEqual(node.value, "Test")
+        self.assertIsNone(node.props)
+
+    def test_initialization_without_value(self):
+        """
+        Test that a TypeError is raised when a LeafNode is
+        initialized without a value.
+        """
+        with self.assertRaises(TypeError):
+            LeafNode("p")
+    
+    def test_initialization_without_data(self):
+        """
+        Test that a TypeError is raised when a LeafNode is
+        initialized without any data.
+        """
+        with self.assertRaises(TypeError):
+            LeafNode()
+    
+    # --- to_html() ---
+    def test_to_html_with_all_data(self):
+        """
+        Test that to_html() returns the correct HTML string when
+        all data is passed in.
+        """
+        props = {
+            "href": "example.html",
+            "target": "_blank"
+        }
+        node = LeafNode("a", "Test", props)
+        expected = '<a href="example.html" target="_blank">Test</a>'
+        self.assertEqual(node.to_html(), expected)
+    
+    def test_to_html_without_props(self):
+        """
+        Test that to_html() returns the correct HTML string when
+        a node has no properties.
+        """
+        node = LeafNode("h1", "Test")
+        expected = "<h1>Test</h1>"
+        self.assertEqual(node.to_html(), expected)
+
+    def test_to_html_without_tag(self):
+        """
+        Test that to_html() returns the value in raw text when
+        a node has no tag.
+        """
+        node = LeafNode(None, "Test")
+        self.assertEqual(node.to_html(), "Test")
+
+    def test_to_html_without_value(self):
+        """
+        Test that to_html() raises a ValueError when value is set to None.
+        """
+        with self.assertRaises(ValueError):
+            LeafNode("h2", None).to_html()
