21688124c6c7fb1cf613be3df44286deadb44a83
Vick Javariz <66493562+javavick@users.noreply.github.com>
2025-03-08 12:38:34 -0500

    Create LeafNode class

---
+++ src/leafnode.py
@@ -0,0 +1,15 @@
+from htmlnode import HTMLNode
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError
+        if self.tag is None:
+            return f"{self.value}"
+
+        props = " " + self.props_to_html() if self.props else ""
+        return f"<{self.tag}{props}>{self.value}</{self.tag}>"
