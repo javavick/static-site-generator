1a205621a9555501cfd50a1aac8e997ca98e4a52
Vick Javariz <66493562+javavick@users.noreply.github.com>
2025-04-01 18:07:17 -0400

    Create block_to_block_type() method

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,16 @@
 import re
 
+from enum import Enum
+
+
+class BlockType(Enum):
+    HEADER = "header"
+    PARAGRAPH = "paragraph"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 
 def markdown_to_blocks(markdown):
     stripped_markdown = markdown.strip('\n')
@@ -9,3 +20,39 @@ def markdown_to_blocks(markdown):
         blocks
     )
     return list(filtered_blocks)
+
+
+def block_to_block_type(block):
+    if not block:
+        raise ValueError("Block cannot be empty.")
+    
+    if re.match(r"^(#{1,6})\s(.+)$", block):
+        return BlockType.HEADER
+    
+    if block[:3] == "```" and block[-3:] == "```":
+        return BlockType.CODE
+    
+    if block.startswith(">"):
+        if "\n" in block:
+            for line in block.split("\n"):
+                if ">" not in line:
+                    return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    
+    if block.startswith("- "):
+        if "\n" in block:
+            for line in block.split("\n"):
+                if not line.startswith("- "):
+                    return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    
+    if block.startswith("1. "):
+        if "\n" in block:
+            item_number = 1
+            for line in block.split("\n"):
+                if not line.startswith(f"{item_number}. "):
+                    return BlockType.PARAGRAPH
+                item_number += 1
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,10 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import (
+    BlockType,
+    block_to_block_type,
+    markdown_to_blocks
+)
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -53,3 +57,169 @@ This is a paragraph.
         expected = ["# This is a heading"]
 
         self.assertEqual(blocks, expected)
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_empty_block(self):
+        """Test that an empty block raises a ValueError."""
+        with self.assertRaises(ValueError):
+            block_to_block_type("")
+    
+    def test_heading_1_block(self):
+        """Test that a heading 1 block is correctly identified."""
+        block = "# This is a heading"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.HEADER)
+    
+    def test_heading_2_block(self):
+        """Test that a heading 2 block is correctly identified."""
+        block = "## This is a heading"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.HEADER)
+    
+    def test_heading_3_block(self):
+        """Test that a heading 3 block is correctly identified."""
+        block = "### This is a heading"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.HEADER)
+    
+    def test_heading_4_block(self):
+        """Test that a heading 4 block is correctly identified."""
+        block = "#### This is a heading"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.HEADER)
+    
+    def test_heading_5_block(self):
+        """Test that a heading 5 block is correctly identified."""
+        block = "##### This is a heading"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.HEADER)
+    
+    def test_heading_6_block(self):
+        """Test that a heading 6 block is correctly identified."""
+        block = "###### This is a heading"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.HEADER)
+    
+    def test_heading_7_block(self):
+        """Test that a heading 7 block is identified as a regular paragraph."""
+        block = "####### This is not a heading"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+    
+    def test_heading_block_without_space(self):
+        """
+        Test that a heading without a space is identified as
+        a regular paragraph.
+        """
+        block = "#This is not a heading"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+    
+    def test_heading_block_without_word(self):
+        """
+        Test that a heading without a word is identified as
+        a regular paragraph.
+        """
+        block = "# "
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+    
+    def test_code_block(self):
+        """Test that a quote block is correctly identified."""
+        block = "```\nThis is a code block\n```"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.CODE)
+    
+    def test_quote_block(self):
+        """Test that a quote block is correctly identified."""
+        block = "> This is a quote"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.QUOTE)
+    
+    def test_quote_block_with_multiple_quotes(self):
+        """
+        Test that a quote block with multiple quotes is correctly identified.
+        """
+        block = "> This is a quote\n> with multiple lines"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.QUOTE)
+    
+    def test_unordered_list_block_single_item(self):
+        """
+        Test that an unordered list block with a single item is
+        correctly identified.
+        """
+        block = "- This is an unordered list"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+    
+    def test_unordered_list_block_multiple_items(self):
+        """
+        Test that an unordered list block with multiple items is
+        correctly identified.
+        """
+        block = "- This is an unordered list\n- with multiple items"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+    
+    def test_unordered_list_invalid_block(self):
+        """
+        Test that an unordered list block with invalid items is
+        identified as a regular paragraph.
+        """
+        block = "- This is an unordered list\nThis is not a list item"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+    
+    def test_ordered_list_block_single_item(self):
+        """
+        Test that an ordered list block with a single item is
+        correctly identified.
+        """
+        block = "1. This is an ordered list"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+    
+    def test_ordered_list_block_multiple_items(self):
+        """
+        Test that an ordered list block with multiple items is
+        correctly identified.
+        """
+        block = "1. This is an ordered list\n2. with multiple\n3. items"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+    
+    def test_ordered_list_invalid_block(self):
+        """
+        Test that an ordered list block with invalid items is
+        identified as a regular paragraph.
+        """
+        block = "1. This is an ordered list\nThis is not a list item"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+    
+    def test_paragraph_block(self):
+        """Test that a paragraph block is correctly identified."""
+        block = "This is a paragraph"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
